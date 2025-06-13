25e72d8458c56ef593fff1b7130e45d68723da58
Kim Jaejun <58386103+rlawlwhs@users.noreply.github.com>
2025-06-13 15:03:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
   $(function() {
     $.get("/data.csv", function(data) {
+      console.log('hello')
       const lines = data.trim().split("\n").slice(1); // 헤더 제외
       lines.forEach(line => {
         const [name, price] = line.split(",");
