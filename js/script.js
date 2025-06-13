006219f5490f35b128e1a1d44c36f511da107439
Kim Jaejun <58386103+rlawlwhs@users.noreply.github.com>
2025-06-13 14:52:27 +0900

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+fetch("data.csv")
+  .then(response => response.text())
+  .then(text => {
+    const rows = text.trim().split("\n").slice(1);
+    const ul = document.getElementById("product-list");
+
+    rows.forEach(row => {
+      const [name, price] = row.split(",");
+      const li = document.createElement("li");
+      li.textContent = `${name} - ${price}원`;
+      ul.appendChild(li);
+    });
+  });
